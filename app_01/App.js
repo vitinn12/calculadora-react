5072edfefbc8970bac3312d3bef616a29f365d0a
Vitor Emanuel da Silva Alves <20231198060029@ifrn.local>
2025-06-26 13:44:06 -0300

    fazendo a estrutura inicial da calculadora. criando as rotas sem as funcoes passadas pelo professor no documento.

---
+++ app_01/App.js
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Calculadora from './comps/Calculadora';
+import CalculadoraCientifica from './comps/Calculadoracientifica';
+
+function Home() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text onPress={() => navigation.navigate('Calculadora')}>Pagina inicial</Text>
+        <View>
+          <Text onPress={() => navigation.navigate('Calculadora')}>Calculadora</Text>
+          <Text onPress={() => navigation.navigate('Calculadora Cientifica')}>Calculadora Cientifica</Text>
+        </View>
+
+      </View>
+
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Calculadora" component={Calculadora} />
+        <Drawer.Screen name="Calculadora Cientifica" component={CalculadoraCientifica} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
